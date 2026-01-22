4341abbd50e6fbf4ecc956bba783e8c498fa14f5
Yohann Le Coguic <yohann.lecoguic@gmail.com>
2026-01-22 18:24:13 +0100

    ajout historique de commandes et detail des commandes

--- src/Modules/Mod_Compte/compte_modele.php
+++ src/Modules/Mod_Compte/compte_modele.php
@@ -29,4 +29,34 @@ class compte_modele extends Connexion {
         $sql->execute([$_SESSION['id_compte']]);
         return $sql->fetch();
     }
+
+    public function getCommandes(){
+        $sql = self::$bdd->prepare("SELECT lc.id_lignecmd, lc.date, prix_total
+                                    FROM Lignecommande lc
+                                    INNER JOIN Passercommande pc ON lc.id_lignecmd = pc.id_lignecmd
+                                    WHERE id_compte = (?)
+                                    ORDER BY lc.id_lignecmd DESC");
+        $sql->execute([$_SESSION['id_compte']]);
+        return $sql->fetchAll();
+    }
+
+    public function getTotalDepense(){
+        $sql = self::$bdd->prepare("SELECT SUM(prix_total) AS total
+                                    FROM Lignecommande lc
+                                    INNER JOIN Passercommande pc ON lc.id_lignecmd = pc.id_lignecmd
+                                    WHERE id_compte = ?
+                                    ");
+        $sql->execute([$_SESSION['id_compte']]);
+        return $sql->fetchColumn();
+    }
+
+    public function getDetailCommande($idCommande){
+        $sql = self::$bdd->prepare("SELECT nom, prix, quantite
+                                    FROM Lignecommande lc
+                                    INNER JOIN Commander c ON lc.id_lignecmd = c.id_lignecmd
+                                    INNER JOIN Produit p ON c.id_produit = p.id
+                                    WHERE lc.id_lignecmd = (?) AND lc.statut like 'payee'");
+        $sql->execute([$idCommande]);
+        return $sql->fetchAll();
+    }
 }
